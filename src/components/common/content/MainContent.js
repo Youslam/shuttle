3c4abeff0be2388c50cd9f7dc92992c467b647c6
yaitlaydi <youssef.aitlaydi@gmail.com>
2019-06-14 17:29:33 +0100

    add Components

---
+++ src/components/common/content/MainContent.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+import './Map.css'
+
+class MainContent extends Component {
+    render () {
+        return (
+            <div id='map' className="map"></div>
+        )
+    }
+}
+
+export default MainContent;
---
+++ src/components/common/footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () => {
+    return (
+        <div className="copyrights">
+            <p>© 2016 Shoppy. All Rights Reserved | Design by  <a href="http://w3layouts.com/" target="_blank" rel="noopener noreferrer">W3layouts</a> </p>
+        </div>
+    )
+}
+
+export default Footer
